b8fcc45b8b6f4bdcedc38a7b00baea63b155f394
Ayush Gupta <ayush.keshav2004@gmail.com>
2024-05-21 20:39:07 +0530

    chore: optimization pass

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,9 @@ struct PathProperties {
 }
 type PathHandlerMap = Arc<HashMap<&'static str, PathProperties>>;
 
-#[derive(Debug, Clone)]
-struct Request {
-    method: String,
-    path: String,
-    headers: HashMap<String, String>,
-    body: Option<String>,
-}
 #[derive(Debug)]
 struct RequestState {
-    stream: TcpStream,
-    request: Option<Request>,
+    conn_stream: TcpStream,
     request_buffer: [u8; 1024],
 }
 struct AppState {
@@ -46,10 +38,10 @@ fn main() {
         (500, "Internal Server Error"),
     ]);
     let routes = Arc::new(HashMap::from([(
-        "/signup",
+        "/",
         PathProperties {
             handler: handle_path_status as fn(AppState) -> (),
-            method_conent_map: HashMap::from([("POST", "application/json")]),
+            method_conent_map: HashMap::from([("GET", "application/json")]),
         },
     )]));
 
@@ -59,111 +51,127 @@ fn main() {
     // handle incoming connections
     let thread_pool = thread::spawn(move || {
         for stream in server.incoming() {
-            let mut state = AppState {
-                status_code_response_map: status_code_response_map.clone(),
-                routes: routes.clone(),
-                request_state: RequestState {
-                    stream: stream.expect("Could not establish connection"),
-                    request: None,
-                    request_buffer: [32; 1024],
-                },
-            };
-            thread::spawn(move || {
-                state
-                    .request_state
-                    .stream
-                    .read(&mut state.request_state.request_buffer)
-                    .unwrap();
-
-                // ["POST / HTTP/1.1", "Host: 127.0.0.1:3000", "User-Agent: curl/7.88.1", "Accept: */*", "Content-Length: 17", "Content-Type: application/x-www-form-urlencoded", "", "{'field': 'data'}"]
-                let request = String::from_utf8_lossy(&state.request_state.request_buffer[..]);
-                let request_lines: Vec<&str> = request.trim().split("\r\n").collect();
-
-                // parse request info.
-                let request_info = request_lines[0].split_whitespace().collect::<Vec<&str>>();
-                if request_info.len() != 3 {
-                    return_response(state, 400, None);
-                    return;
-                }
-                state.request_state.request = Some(Request {
-                    method: request_info[0].to_string(),
-                    path: request_info[1].to_string(),
-                    headers: HashMap::new(),
-                    body: None,
-                });
-
-                // parse headers.
-                let mut header_iterator = request_lines.iter().skip(1);
-                while let Some(line) = header_iterator.next() {
-                    if line.is_empty() {
-                        break;
-                    }
-                    let split_header: Vec<&str> = line.split(": ").collect();
+            if let Some(stream) = stream.ok() {
+                let mut state = AppState {
+                    status_code_response_map: status_code_response_map.clone(),
+                    routes: routes.clone(),
+                    request_state: RequestState {
+                        conn_stream: stream,
+                        request_buffer: [32; 1024],
+                    },
+                };
+                thread::spawn(move || {
                     state
                         .request_state
-                        .request
-                        .as_mut()
-                        .unwrap()
-                        .headers
-                        .insert(split_header[0].to_string(), split_header[1].to_string());
-                }
+                        .conn_stream
+                        .read(&mut state.request_state.request_buffer)
+                        .unwrap();
 
-                // parse body.
-                let body = request_lines.last().unwrap_or(&"");
-                if !body.is_empty() {
-                    state
-                        .request_state
-                        .request
-                        .as_mut()
-                        .unwrap()
-                        .body
-                        .replace(body.to_string());
-                }
+                    // ["POST / HTTP/1.1", "Host: 127.0.0.1:3000", "User-Agent: curl/7.88.1", "Accept: */*", "Content-Length: 17", "Content-Type: application/x-www-form-urlencoded", "", "{'field': 'data'}"]
+                    let request = String::from_utf8_lossy(&state.request_state.request_buffer[..]);
+                    let request_lines: Vec<&str> = request.trim().split("\r\n").collect();
 
-                // handle path
-                let path = state.request_state.request.as_ref().unwrap().path.as_str();
-                let path_properties = match state.routes.get(path) {
-                    Some(properties) => properties,
-                    None => {
-                        handle_404(state);
+                    // parse info
+                    let request_info = request_lines[0].split_whitespace().collect::<Vec<&str>>();
+                    if request_info.len() != 3 {
+                        return_response(state, 400, None, None);
                         return;
                     }
-                };
 
-                // handle methods that aren't allowed
-                if !path_properties
-                    .method_conent_map
-                    .contains_key(request_info[0])
-                {
-                    return_response(state, 405, None);
-                    return;
-                }
+                    // parse headers.
+                    let headers = request_lines
+                        .iter()
+                        .skip(1)
+                        .take_while(|line| !line.is_empty())
+                        .collect::<Vec<&&str>>()
+                        .pop();
+                    if headers.is_none() {
+                        return_response(state, 400, None, None);
+                        return;
+                    }
 
-                (path_properties.handler)(state);
-            });
+                    // parse body.
+                    // let body = request_lines.last();
+
+                    // handle path
+                    let path_properties = match state.routes.get(request_info[1]) {
+                        Some(properties) => properties,
+                        None => {
+                            handle_404(state);
+                            return;
+                        }
+                    };
+
+                    // handle methods that aren't allowed
+                    if !path_properties
+                        .method_conent_map
+                        .contains_key(request_info[0])
+                    {
+                        return_response(state, 405, None, None);
+                        return;
+                    }
+
+                    (path_properties.handler)(state);
+                });
+            }
         }
     });
     thread_pool.join().unwrap();
 }
 
-fn return_response(mut state: AppState, status_code: u16, body: Option<Value>) {
+fn return_response(
+    mut state: AppState,
+    code: u16,
+    headers: Option<HashMap<&str, &str>>,
+    body: Option<Value>,
+) {
+    let response_body = body.unwrap_or(json!({})).to_string();
+    let response_body_len = response_body.len().to_string();
     let response = format!(
-        "HTTP/1.1 {} {}\r\n\r\n{}",
-        status_code,
-        state.status_code_response_map.get(&status_code).unwrap(),
-        body.unwrap_or(json!({})).to_string()
+        "HTTP/1.1 {} {}\n{}\r\n\r\n{}",
+        code,
+        state.status_code_response_map.get(&code).unwrap(),
+        {
+            let mut headers = headers.unwrap_or(HashMap::new());
+            if !headers.contains_key("Content-Type") {
+                headers.insert("Content-Type", "application/json");
+            }
+            if !headers.contains_key("Content-Length") {
+                headers.insert("Content-Length", &response_body_len);
+            }
+            if !headers.contains_key("Connection") {
+                headers.insert("Connection", "close");
+            }
+            if !headers.contains_key("Server") {
+                headers.insert("Server", "Hotpocket :3");
+            }
+            if !headers.contains_key("X-Frame-Options") {
+                headers.insert("X-Frame-Options", "SAMEORIGIN");
+            }
+            headers
+                .iter()
+                .map(|(key, value)| format!("{}: {}\r\n", key, value))
+                .collect::<String>()
+                .trim()
+        },
+        response_body
     );
     state
         .request_state
-        .stream
+        .conn_stream
         .write(response.as_bytes())
         .unwrap();
 }
 
 fn handle_path_status(state: AppState) {
-    return_response(state, 200, None);
+    return_response(state, 200, None, None);
 }
 
 fn handle_404(state: AppState) {
-    return_response(state, 404, None);
+    return_response(
+        state,
+        404,
+        None,
+        Some(json!({"error": "The requested resource was not found."})),
+    );
 }
